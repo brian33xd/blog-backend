ffa9d28e9f5101194b9fd18a1682663fdc930449
Brian Godoy <briangodoy01@outlook.com>
2023-12-22 13:53:56 -0300

    v4

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
+const mongo_URI =
+  "mongodb+srv://BrianDB:brian1110011@brian33.sn9ep8h.mongodb.net/Blog";
+
 const connection = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://BrianDB:brian1110011@brian33.sn9ep8h.mongodb.net/Blog",
-      {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(mongo_URI, {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("Correctly connected to database my blog");
   } catch (error) {
